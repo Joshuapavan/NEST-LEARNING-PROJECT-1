e4b00b07c913b8eae1cd483b77b12213fecb086b
Joshua Pavan <joshuapavan35@gmail.com>
2025-05-03 11:46:44 +0530

    Adds changes for the index

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hey 👋🏻, Welcome to my User Management API';
   }
 }
